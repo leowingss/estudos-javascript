9d45b2758f5b2e34b97b71339a9c2c9e5c745f83
Leonardo Santos Gonçales <leondani853@gmail.com>
2021-06-27 23:48:40 -0300

    feat: arquivo aula 06

---
+++ aula06/script copy.js
@@ -0,0 +1,18 @@
+// function alerta() {
+//      window.alert('Minha primeira mensagem')
+//  }
+
+//  alerta()
+
+//  function confirmar() {
+//      window.confirm('Está gostando de JavaScript?')
+//  }
+
+// confirmar()
+
+function seuNome() {
+    let nome = window.prompt('Qual seu nome?')
+    alert('Prazer em te conhecer, ' + nome + ' !')
+}
+
+seuNome()
---
+++ aula06/script02.js
@@ -0,0 +1,4 @@
+let n1 = parseFloat(window.prompt('Digite um número'))
+let n2 = parseFloat(window.prompt('Digite outro número'))
+let soma = n1 + n2
+alert('A soma dos valores é: ' + soma)
