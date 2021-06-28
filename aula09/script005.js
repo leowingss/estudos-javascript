58c7efd5e8230d28e61200e9c8f73d0eae1588d5
Leonardo Santos Gonçales <leondani853@gmail.com>
2021-06-28 14:54:37 -0300

    feat: arquivo aula 09

---
+++ aula09/script005.js
@@ -0,0 +1,10 @@
+let corpo = document.body
+let p1 = document.getElementsByTagName('p')[1]
+    // let d = document.getElementById('msg')
+
+// d.style.background = 'red'
+//  d.innerText = 'Estou aguardando...'
+
+let d = document.querySelector('div.msg')
+
+d.style.background = 'red'
