7b4b44b6083a4c8b35b8a3e063b7df232490914b
Leonardo Santos Gonçales <leondani853@gmail.com>
2021-06-30 13:47:28 -0300

    feat: aula 13- while

---
+++ aula13/ambiente.js
@@ -0,0 +1,20 @@
+// WHILE ---------------------------
+
+// let contador = 1
+
+// while (contador < 6) {
+//     console.log(`Volta ${contador}`)
+//     contador++
+// }
+
+// WHILE ---------------------------
+
+
+// DO WHILE --------------
+
+let contador = 1
+
+do {
+    console.log(`Volta ${contador}`)
+    contador++
+} while (contador <= 5)
