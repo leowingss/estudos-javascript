9f45dbc11fe25abfbe98d12c3cc9a6e4a5af9ff7
Leonardo Santos Gonçales <leondani853@gmail.com>
2021-07-01 18:11:51 -0300

    feat: aula funções

---
+++ aula16/funcao01.js
@@ -0,0 +1,12 @@
+function parImpar (numero) {
+
+    if(numero % 2 == 0){
+        return `O número: ${numero} é Par!`
+    } else {
+        return `O número: ${numero} é Ímpar!`
+    
+    }
+
+}
+
+console.log(parImpar(12.5))
