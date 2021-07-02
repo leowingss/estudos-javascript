86ecd9ae59c6f80b7b37cf9144cb6a4e7f278d31
Leonardo Santos Gonçales <leondani853@gmail.com>
2021-07-02 02:55:43 -0300

    feat: aula 17 - objetos

---
+++ aula17/objeto01.js
@@ -0,0 +1,11 @@
+let amigo = {
+    nome: 'José',
+    sexo: 'Masculino',
+    peso: 85.4,
+    engordar(p = 0) {
+        console.log('Engordou')
+        this.peso += p
+    }
+}
+amigo.engordar(4)
+console.log(`O ${amigo.nome} pesa ${amigo.peso}kg. `)
